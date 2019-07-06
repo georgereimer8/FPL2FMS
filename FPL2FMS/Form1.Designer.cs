8e80d50618645d355c5019b89a044452c398be50
George Reimer <georgereimer@live.ca>
2019-07-05 22:59:47 -0400

    made all waypoints including airports DRCT

--- FPL2FMS/Form1.Designer.cs
+++ FPL2FMS/Form1.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button_load = new System.Windows.Forms.Button();
+            this.textBox_load = new System.Windows.Forms.TextBox();
+            this.textBox_fpl = new System.Windows.Forms.TextBox();
+            this.textBox_fms = new System.Windows.Forms.TextBox();
+            this.textBox_save = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox_fpl);
+            this.groupBox1.Controls.Add(this.textBox_load);
+            this.groupBox1.Controls.Add(this.button_load);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(645, 254);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "FPL";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox_fms);
+            this.groupBox2.Controls.Add(this.textBox_save);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 254);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(645, 233);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "FMS";
+            // 
+            // button_load
+            // 
+            this.button_load.Location = new System.Drawing.Point(12, 211);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(75, 23);
+            this.button_load.TabIndex = 0;
+            this.button_load.Text = "Load";
+            this.button_load.UseVisualStyleBackColor = true;
+            this.button_load.Click += new System.EventHandler(this.button_load_Click);
+            // 
+            // textBox_load
+            // 
+            this.textBox_load.Location = new System.Drawing.Point(94, 213);
+            this.textBox_load.Name = "textBox_load";
+            this.textBox_load.Size = new System.Drawing.Size(519, 20);
+            this.textBox_load.TabIndex = 1;
+            // 
+            // textBox_fpl
+            // 
+            this.textBox_fpl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox_fpl.Location = new System.Drawing.Point(3, 16);
+            this.textBox_fpl.Multiline = true;
+            this.textBox_fpl.Name = "textBox_fpl";
+            this.textBox_fpl.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_fpl.Size = new System.Drawing.Size(639, 189);
+            this.textBox_fpl.TabIndex = 2;
+            this.textBox_fpl.Text = "FPL to FMS Converter\r\nJuly 3,2019\r\nGeorge Reimer\r\nPublic Release\r\n\r\nConvert SkyVe" +
+    "ctor FPL files to XPlane 11 FMS files.";
+            // 
+            // textBox_fms
+            // 
+            this.textBox_fms.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox_fms.Location = new System.Drawing.Point(3, 16);
+            this.textBox_fms.Multiline = true;
+            this.textBox_fms.Name = "textBox_fms";
+            this.textBox_fms.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_fms.Size = new System.Drawing.Size(639, 180);
+            this.textBox_fms.TabIndex = 5;
+            // 
+            // textBox_save
+            // 
+            this.textBox_save.Location = new System.Drawing.Point(94, 204);
+            this.textBox_save.Name = "textBox_save";
+            this.textBox_save.Size = new System.Drawing.Size(519, 20);
+            this.textBox_save.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "*.fpl";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "txt files (*.fpl)|*.fpl|All files (*.*)|*.*";
+            this.openFileDialog1.InitialDirectory = "H:\\SteamLibrary\\steamapps\\common\\X-Plane 11\\Output\\FMS plans";
+            this.openFileDialog1.Title = "Load FPL File";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "*.fms";
+            this.saveFileDialog1.Filter = "txt files (*.fms)|*.fms|All files (*.*)|*.*";
+            this.saveFileDialog1.InitialDirectory = "H:\\SteamLibrary\\steamapps\\common\\X-Plane 11\\Output\\FMS plans";
+            this.saveFileDialog1.Title = "Save FMS file";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(645, 487);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "FPL to FMS Converter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_fpl;
+        private System.Windows.Forms.TextBox textBox_load;
+        private System.Windows.Forms.Button button_load;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox_fms;
+        private System.Windows.Forms.TextBox textBox_save;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- FPL2FMS/Form1.cs
+++ FPL2FMS/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace FPL2FMS
 {
@@ -15,6 +17,119 @@ namespace FPL2FMS
         public Form1()
         {
             InitializeComponent();
+        }
+
+        XDocument fpl;
+        private void button_load_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                try
+                {
+
+                    fpl = XDocument.Load(openFileDialog1.FileName, LoadOptions.PreserveWhitespace);
+                    textBox_fpl.Text = fpl.Root.ToString();
+                    textBox_load.Text = openFileDialog1.FileName;
+                    convert();
+                    textBox_save.Text = Path.ChangeExtension(openFileDialog1.FileName, "fms");
+                }
+                catch (System.Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        string elementValue(XElement e, string name)
+        {
+            string value = "";
+            var t = (from x in e.Descendants()
+                     where x.Name.LocalName.Equals(name)
+                     select x).ToList();
+            if( t.Count > 0 )
+            {
+                value = t[0].Value;
+            }
+            return value;
+        }
+
+        string fms;
+        bool convert()
+        {
+            bool result = false;
+            textBox_fms.Clear();
+            textBox_fms.AppendText("I" + Environment.NewLine);
+            textBox_fms.AppendText("1100 Version" + Environment.NewLine);
+            textBox_fms.AppendText("CYCLE 1708" + Environment.NewLine);
+
+            var waypoints = (from wp in fpl.Root.Descendants()
+                             where wp.Name.LocalName == "waypoint"
+                             select wp).ToList();
+
+            List<string> airports = new List<string>();
+            var s = (String.Format("NUMENR {0}{1}", waypoints.Count, Environment.NewLine));
+            bool depAdded = false;
+            foreach (var w in waypoints)
+            {
+                var s1 = "";
+                var t = elementValue(w, "type");
+                if (t == "AIRPORT" ) 
+                {
+                    if (depAdded == false )
+                    {
+                        //s1 = "1 " + elementValue(w, "identifier") + " ADEP ";
+                        s1 = "1 " + elementValue(w, "identifier") + " DRCT ";
+                        depAdded = true;
+                    }
+                    else
+                    {
+                        //s1 = "1 " + elementValue(w, "identifier") + " ADES ";
+                        s1 = "1 " + elementValue(w, "identifier") + " DRCT ";
+                    }
+                    airports.Add(elementValue(w, "identifier"));
+                }
+                else if (t == "NDB")
+                {
+                    s1 = "2 " + elementValue(w, "identifier") + " DRCT ";
+                }
+                else if (t == "VOR")
+                {
+                    s1 = "3 " + elementValue(w, "identifier") + " DRCT ";
+                }
+                else if (t == "INT")
+                {
+                    s1 = "11 " + elementValue(w, "identifier") + " DRCT ";
+                }
+                else
+                {
+                    s1 = "28 " + elementValue(w, "identifier") + " DRCT ";
+                }
+
+                s1 += "0.000000 ";
+                s1 += elementValue(w, "lat") + " ";
+                s1 += elementValue(w, "lon") + " ";
+                s1 += Environment.NewLine;
+
+                s += s1;
+            }
+            if (airports.Count > 0)
+            {
+                textBox_fms.AppendText("ADEP " + airports[0] + Environment.NewLine);
+                textBox_fms.AppendText("ADES " + airports.Last() + Environment.NewLine);
+            }
+            textBox_fms.AppendText(s);
+            return result;
+        } 
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = Path.GetDirectoryName(textBox_save.Text);
+            saveFileDialog1.FileName = textBox_save.Text;
+            if( saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, textBox_fms.Text);
+            }
         }
     }
 }
